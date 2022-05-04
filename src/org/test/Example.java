9b4ffb34238bdc80ebf065183b0f88c75e0f7529
sakthigitlogin <sakthigitlogin@gmail.com>
2022-05-04 13:37:18 +0530

    A added few lines of code

--- src/org/test/Example.java
+++ src/org/test/Example.java
@@ -1,12 +1,15 @@
 package org.test;
 
 public class Example {
-	
-	int a=100;
-	int b=20;
+
+	int a = 100;
+
+	public void add(int a, int b) {
+		int c = a + b;
+		System.out.println(c);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("main method");
 	}
 }
-
