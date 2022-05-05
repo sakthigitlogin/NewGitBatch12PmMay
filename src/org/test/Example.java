5dfc93d191e040b935d34340a25d7226eb56f1c8
sakthigitlogin <sakthigitlogin@gmail.com>
2022-05-05 12:56:41 +0530

    Person B added few lines

--- src/org/test/Example.java
+++ src/org/test/Example.java
@@ -4,6 +4,10 @@ public class Example {
 	
 	int a=100;
 	int b=20;
+	
+	private void display() {
+		System.out.println("B method ");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("main method");
